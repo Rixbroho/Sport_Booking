addea0a30c25f560a1e0b02bafa44dc15900b684
Jay-9121 <pradhanjay911@gmail.com>
2025-12-18 11:55:08 +0545

    index Changes

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,23 @@
-const express=require('express');
-const app=express();
-const port=3000;
+const express = require('express');
+const app = express();
+const {squelize, connectDB, sequelize} = require('./database/database');
 
-app.use("/api/user/",require("./routes/route"));
+app.use("/api/user/", require("./routes/route"));
+app.use("/api/user/", require("./routes/productRoute"));
 
-
-
-app.get('/',(req,res)=>{
-    res.json({message:'Welcome to Sport Book API'});
-});
-app.get('/getAllusers',(req,res)=>{
-    res.json({message:'Welcome to users'});
+app.get('/', (req, res) => {
+    res.json('Welcome to the home page');
 });
 
-app.listen(port,()=>{
-    console.log(`Server is running at http://localhost:${port}`);
-});
+// app.listen(3000, () => {
+//     console.log('Server is running on http://localhost:3000');
+// });
+
+const startServer = async () => {
+    await connectDB();
+    await sequelize.sync({ alter: true }); // Sync models with the database
+    app.listen(3000, () => {
+        console.log('Server is running on http://localhost:3000');
+    });
+}
+// startServer();
